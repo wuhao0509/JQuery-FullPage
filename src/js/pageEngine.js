04264ac740c240fc249d0ea2479a6e4f8325cedb
wuhao0509 <303459939@qq.com>
2020-12-03 01:11:24 +0800

    myFullPage

--- src/js/pageEngine.js
+++ src/js/pageEngine.js
@@ -1,66 +1,66 @@
-var pageEngine = {
-    // $w
-    // colorsArray
-    // slideFlag
-    // $Page
-    // $Slide
-    init: function (selector, colorsArray) {
-      this.$w = $(selector);
-      this.colorsArray = colorsArray;
-      this.slideFlag = false;
-      return this;
-    },
-    addSection: function (className) {
-      this.slideFlag = false;
-      this.$Page = $('<div class="section"></div>').addClass(className);
-      this.$Page.appendTo(this.$w);
-      return this;
-    },
-    addSlide: function (className) {
-      this.slideFlag = true;
-      this.$Slide = $('<div class="slide"></div>').addClass(className);
-      this.$Slide.appendTo(this.$Page);
-      return this;
-    },
-    addComponent: function (config) {
-      var type = config.type;
-      var component = null;
-      switch (type) {
-        case "base":
-          component = ComponentFactory(config);
-          break;
-        case "super":
-          component = ComponentSuperFactory(config);//其他的用户自己写的定制元素的方法
-          break;
-      }
-      this.slideFlag? this.$Slide.append(component): this.$Page.append(component);
-      return this;
-    },
-    bindEvent: function () {
-      this.$w.find(".section").on({
-        _leave: function () {
-          $(this).find(".component").trigger("cpLeave");
-        },
-        _load: function () {
-          $(this).find(".component").trigger("cpLoad");
-        },
-      });
-    },
-    load: function () {
-      this.bindEvent();
-      var self = this;
-      this.$w.myFullPage({
-        colorsArray: this.colorsArray,
-        onLeave: function (index, direction) {
-          // 找到离开section
-          self.$w.find(".section").eq(index).trigger("_leave");
-        },
-        afterLoad: function (index, direction) {
-          // 来的
-          self.$w.find(".section").eq(index).trigger("_load");
-        },
-      });
-      this.$w.find(".section").eq(0).trigger("_load");
-    },
-  };
+// var pageEngine = {
+//     // $w
+//     // colorsArray
+//     // slideFlag
+//     // $Page
+//     // $Slide
+//     init: function (selector, colorsArray) {
+//       this.$w = $(selector);
+//       this.colorsArray = colorsArray;
+//       this.slideFlag = false;
+//       return this;
+//     },
+//     addSection: function (className) {
+//       this.slideFlag = false;
+//       this.$Page = $('<div class="section"></div>').addClass(className);
+//       this.$Page.appendTo(this.$w);
+//       return this;
+//     },
+//     addSlide: function (className) {
+//       this.slideFlag = true;
+//       this.$Slide = $('<div class="slide"></div>').addClass(className);
+//       this.$Slide.appendTo(this.$Page);
+//       return this;
+//     },
+//     addComponent: function (config) {
+//       var type = config.type;
+//       var component = null;
+//       switch (type) {
+//         case "base":
+//           component = ComponentFactory(config);
+//           break;
+//         case "super":
+//           component = ComponentSuperFactory(config);//其他的用户自己写的定制元素的方法
+//           break;
+//       }
+//       this.slideFlag? this.$Slide.append(component): this.$Page.append(component);
+//       return this;
+//     },
+//     bindEvent: function () {
+//       this.$w.find(".section").on({
+//         _leave: function () {
+//           $(this).find(".component").trigger("cpLeave");
+//         },
+//         _load: function () {
+//           $(this).find(".component").trigger("cpLoad");
+//         },
+//       });
+//     },
+//     load: function () {
+//       this.bindEvent();
+//       var self = this;
+//       this.$w.myFullPage({
+//         colorsArray: this.colorsArray,
+//         onLeave: function (index, direction) {
+//           // 找到离开section
+//           self.$w.find(".section").eq(index).trigger("_leave");
+//         },
+//         afterLoad: function (index, direction) {
+//           // 来的
+//           self.$w.find(".section").eq(index).trigger("_load");
+//         },
+//       });
+//       this.$w.find(".section").eq(0).trigger("_load");
+//     },
+//   };
   
--- src/js/tool/myFullPage.js
+++ src/js/tool/myFullPage.js
@@ -1,191 +1,179 @@
-// fullPage插件实际上是给jQuery添加一个实例方法
 $.fn.extend({
-    myFullPage: function (config) {
-        var colorsArray = config.colorsArray;
-        // $(this)是一个jQuery对象，如果每次用$(this)，那么每次都会执行$函数，耗费性能，
-        // 所以我们可以将一个$(this)赋值给一个变量 这个变量是一个引用值，后面使用$(this)时，
-        // 都使用的是第一个$(this)
-        var $w = $(this);
-        var $sec = $w.find('.section');
+  myFullPage: function (config) {
+    // 保存this及常用元素
+    var $w = $(this);
+    var $sec = $w.find(".section");
+    var colorsArray = config.colorsArray;
+    var clientHeight = $(window).outerHeight();
+    var clientWidth = $(window).outerWidth();
+    var commonStyle = {
+      width: "100%",
+      height: "100%",
+    };
+    var curIndex = 0;
+    var lock = true;
 
+    // 初始化样式
 
-        // 获取边框盒宽高
-        var clientWidth = $(window).outerWidth();
-        var clientHeight = $(window).outerHeight();
+    // 给html body wrapper section添加共同样式
+    $("html")
+      .add("body")
+      .css({
+        position: "relative",
+        overflow: "hidden",
+        margin: 0,
+      })
+      .add($w)
+      .add($sec)
+      .css(commonStyle);
 
-        // 将使用fullPage插件固定结构的样式中重复的样式封装成一个对象
-        var commonStyle = {
-            width: '100%',
-            height: '100%'
+    // 给wrapper section slide添加特殊样式
+    $w.css({
+      position: "absolute",
+      left: 0,
+      top: 0,
+    })
+      .find($sec)
+      .each(function (index, elem) {
+        $(elem)
+          .css({
+            backgroundColor: colorsArray[index],
+            position: "relative", //sliderWrapper包含块
+          })
+          .find(".slide")
+          .css({
+            float: "left",
+            width: clientWidth,
+            height: clientHeight,
+            position: "relative", //slide里面内容的包含块
+          })
+          .wrapAll('<div class="slideWrapper"></div>');
+      });
+
+    // 设置slideWrapper宽高
+    $sec.find(".slideWrapper").each(function (index, elem) {
+      $(elem).css({
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: $(elem).find(".slide").size() * clientWidth,
+        height: clientHeight,
+      });
+    });
+
+    // 类名初始化
+    $sec
+      .eq(0)
+      .addClass("active")
+      .end() //返回$sec 对$sec继续进行其他的操作
+      .find(".slideWrapper")
+      .each(function (index, elem) {
+        $(elem).find(".slide").eq(0).addClass("innerActive");
+      });
+
+    //给document绑定键盘事件
+    $(document).on("keydown", function (e) {
+      // 垂直运动
+      if (e.which == 38 || e.which == 40) {
+        if (lock) {
+          lock = false;
+          var newTop = $w.offset().top;
+          var direction = "";
+
+          // 上运动
+          if (e.which == 38 && curIndex != 0) {
+            direction = "top";
+            config.onLeave(curIndex,direction);
+            curIndex--;
+            newTop += clientHeight;
+          }
+          // 下运动
+          if (e.which == 40 && curIndex != $sec.size() - 1) {
+            direction = "bottom";
+            config.onLeave(curIndex,direction);
+            curIndex++;
+            newTop -= clientHeight;
+          }
+
+          $w.animate(
+            {
+              top: newTop,
+            },
+            150,
+            function () {
+              lock = true;
+              //给当前section添加active
+              $sec.eq(curIndex).addClass("active");
+              // 给上一个section移除active
+              if (direction == "top") {
+                $sec.eq(curIndex + 1).removeClass("active");
+              } else if (direction == "bottom") {
+                $sec.eq(curIndex - 1).removeClass("active");
+              }
+
+              //此时新页面加载完毕，执行onLoad回调函数,传递参数，当前页面索引值及运动方向
+              config.onLoad(curIndex,direction);
+            }
+          );
         }
+      }
+      // 水平运动
+      if (e.which == 37 || e.which == 39) {
+        if (lock) {
+          lock = false;
+          var $sw = $sec.find(".slideWrapper");
+          //.index()返回元素在其兄弟节点中的索引值
+          var innerActiveIndex = $sw.find(".innerActive").index();
+          var newLeft = $sw.offset().left;
+          var direction = "";
 
-        // 记录当前页面的索引 初始值0
-        var curIndex = 0;
+          //左运动
+          if (e.which == 37 && innerActiveIndex != 0) {
+            direction = "left";
+            config.onLeave(innerActiveIndex,direction);
+            innerActiveIndex--;
+            newLeft += innerWidth;
+          }
 
-        // 加锁
-        var lock = true;
+          //右运动
+          if (
+            e.which == 39 &&
+            innerActiveIndex != $sw.find(".slide").size() - 1
+          ) {
+            direction = "right";
+            config.onLeave(innerActiveIndex,direction);
+            innerActiveIndex++;
+            newLeft -= innerWidth;
+          }
 
-        // 初始化样式
-        // 给元素添加共同样式commonStyle以及单独样式
-        // 某元素单独样式可以在add(该元素)后直接添加
-        // 共同样式可以在所有add之后添加
-        $('html')
-            .add('body')
-            .css({
-                position: 'relative',
-                overflow: 'hidden',
-                margin: 0
-            })
-            .add($w)
-            .add($sec)
-            .css(commonStyle);
+          $sw.animate(
+            {
+              left: newLeft,
+            },
+            150,
+            function () {
+              lock = true;
+              //给当前slide添加innerActive
+              $sw.find(".slide").eq(innerActiveIndex).addClass("innerActive");
+              //找到上一个slide移除innerActive
+              if (direction == "left") {
+                $sw
+                  .find(".slide")
+                  .eq(innerActiveIndex + 1)
+                  .removeClass("innerActive");
+              } else if (direction == "right") {
+                $sw
+                  .find(".slide")
+                  .eq(innerActiveIndex - 1)
+                  .removeClass("innerActive");
+              }
 
-        $w
-            .css({
-                position: 'absolute',
-                left: 0,
-                top: 0
-            })
-            .find('.section')
-            .each(function (index, ele) {
-                // ele是dom元素
-                $(ele)
-                    .css({
-                        backgroundColor: colorsArray[index],
-                        position: 'relative'
-                    })
-                    .find('.slide')
-                    .css({
-                        float: 'left',
-                        width: clientWidth,
-                        height: clientHeight,
-                        position: 'relative'
-                    })
-                    .wrapAll('<div class="slideWrapper"></div>')
-                // 给每一个section下面的slide添加一个父元素，父元素带动slide实现横向移动
-            })
-
-
-        // 设置slideWrapper宽高
-        $sec
-            .find('.slideWrapper')
-            .each(function (index, ele) {
-                $(ele)
-                    .css({
-                        width: $(ele).find('.slide').size() * clientWidth,
-                        height: clientHeight,
-                        position: 'absolute',
-                        left: 0,
-                        top: 0
-                    })
-            })
-
-
-        // js控制移动
-        // 用active标记当前显示的页面
-        // 先给第一个section 给每一个section下面的第一个slide innerActive
-
-        // 类名初始化
-        $sec
-            .eq(0)
-            .addClass('active')
-            .end()
-            .find('.slideWrapper')
-            .each(function (index, elem) {
-                $(elem).find('.slide').eq(0).addClass('innerActive');
-            })
-
-        // 控制移动
-        $(document).on('keydown', function (e) {
-            // 事件对象e中的which记录键盘按键的序号
-            // left 37 top 38 right 39 bottom 40
-
-            // 垂直移动
-            if (e.which == 38 || e.which == 40) {
-                // 移动$w 来带动section
-
-                // 加锁
-                if (lock) {
-                    lock = false;
-                    // 计算wrapper相对于文档顶部的距离 一开始newTop值为0
-                    var newTop = $w.offset().top;
-                    var direction = '';
-                    if (e.which == 38 && curIndex != 0) {
-                        // 先更新index
-                        direction = 'top';
-                        config.onLeave(curIndex,direction);
-                        curIndex--;
-                        newTop += clientHeight;
-                    } else if (e.which == 40 && curIndex != $sec.size() - 1) {
-                        direction = 'bottom';
-                        config.onLeave(curIndex,direction);
-                        curIndex++;
-                        newTop -= clientHeight;
-                    }
-
-                    // 移动wrapper
-                    $w.animate({
-                        top: newTop
-                    }, 350, 'swing', function () {
-                        lock = true;
-
-                        // 给当前页面添加active，同时给上一个页面删除active
-                        $sec.eq(curIndex).addClass('active');
-                        if (direction == 'top') {
-                            $sec.eq(curIndex + 1).removeClass('active');
-                        } else if (direction == 'bottom') {
-                            $sec.eq(curIndex - 1).removeClass('active');
-                        }
-
-                        config.onLoad(curIndex,direction);
-                    })
-                }
-
+              //此时新页面加载完毕，执行onLoad回调函数,传递参数，当前页面索引值及运动方向
+              config.onLoad(innerActiveIndex,direction);
             }
-
-            // 水平移动
-            if (e.which == 37 || e.which == 39) {
-                // 移动slideWrapper 来带动slide
-
-                if (lock) {
-                    lock = false;
-                    var $sw = $('.active').find('.slideWrapper');
-                    var innerActiveIndex = $sw.find('.innerActive').index();
-                    var newLeft = $sw.offset().left;
-                    var direction = '';
-                    if (e.which == 37 && innerActiveIndex != 0) {
-                        // 左移
-                        direction = 'left';
-                        config.onLeave(innerActiveIndex,direction);
-                        innerActiveIndex--;
-                        newLeft += clientWidth;
-                    } else if (e.which == 39 && innerActiveIndex != $sw.find('.slide').size()-1) {
-                        // 右移
-                        direction = 'right';
-                        config.onLeave(innerActiveIndex,direction);
-                        innerActiveIndex++;
-                        newLeft -= clientWidth;
-                    }
-
-                    // 移动slideWrapper
-
-                    $sw.animate({
-                        left: newLeft
-                    }, 350, 'swing', function () {
-                        lock = true;
-
-                        $sw.find('.slide').eq(innerActiveIndex).addClass('innerActive');
-                        if (direction == 'left') {
-                            $sw.find('.slide').eq(innerActiveIndex+1).removeClass('innerActive');
-                        }else if(direction == 'right'){
-                            $sw.find('.slide').eq(innerActiveIndex-1).removeClass('innerActive');
-                        }
-                        config.onLoad(innerActiveIndex,direction);
-                    })
-
-                }
-
-            }
-        })
-    }
-})
+          );
+        }
+      }
+    });
+  },
+});
