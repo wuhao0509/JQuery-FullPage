477339d2e3426caea1120f1d47e3170cf451fa47
wuhao0509 <303459939@qq.com>
2020-12-03 20:45:29 +0800

    完善ComponentFactory函数

--- src/js/component/componentBase.js
+++ src/js/component/componentBase.js
@@ -1,33 +1,29 @@
-// // 生成section里面的内容
-// var ComponentFactory = function (config) {
-//     var $Div = $('<div class="component base"></div>');
-    
-//     // config 定制化 $Div
-//     config.className && $Div.addClass(config.className)
-//     config.width && $Div.css('width', config.width);
-//     config.height && $Div.css('height', config.height);
-//     config.text && $Div.text(config.text);
-//     config.center && $Div.css({position: 'absolute', left: '50%', marginLeft: -config.width / 2});
-//     config.css && $Div.css(config.css);
-//     if (config.event) {
-//         for (var prop in config.event) {
-//             $Div.on(prop, config.event[prop]);
-//         }
-//     }
+var ComponentFactory = function (config) {
+  var $Div = $('<div class="component base"></div>');
 
-//     $Div.on('cpLeave', function () {
-//         var self = this;
-//         setTimeout(function () {
-//             config.animateOut && $(self).animate( config.animateOut );
-//         }, config.delay || 0)
-//     });
+  config.className && $Div.className(config.className);
+  config.content && $Div.append($(config.content));
+  config.css && $Div.css(config.css);
+  if (config.event) {
+    for (var prop in config.event) {
+      $Div.on(prop, config.event[prop]);
+    }
+  }
 
-//     $Div.on('cpLoad', function () {
-//         var self = this
-//         setTimeout(function () {
-//             config.animateIn && $(self).animate( config.animateIn );
-//         }, config.delay || 0)
-//     });
+  var an = config.animation;
+  $Div.on("cpLoad", function () {
+    var self = this;
+    setTimeout(function () {
+      an.animateIn && $(self).animate(an.animateIn, an.movement, an.inCallback());
+    }, an.delay || 0);
+  });
 
-//     return $Div;
-// }
+  $Div.on("cpLeave", function () {
+    var self = this;
+    setTimeout(function () {
+      an.animateOut && $(self).animate(an.animateOut, an.movement, an.outCallback());
+    }, an.delay || 0);
+  });
+
+  return $Div;
+};
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,40 +1,75 @@
 $(".wrapper").myFullPage({
   colorsArray: ["red", "green", "blue", "#008c8c"],
   onLeave: function (index, direction) {
-    $('.section').eq(index).trigger("_leave");
+    $(".section").eq(index).trigger("_leave");
   },
   onLoad: function (index, direction) {
-    $('.section').eq(index).trigger("_load");
+    $(".section").eq(index).trigger("_load");
   },
 });
 
 // 创建元素
-function ComponentFactory() {
-    var $Div = $('<div class="component" style="display: none;"><span></span></div>');
-    $Div.find('span').text('duyi');
-    $Div.on('cpLeave',function(){
-        $(this).fadeOut();
-    })
-    $Div.on('cpLoad',function(){
-        $(this).fadeIn();
-    })
-    return $Div;
-}
+// function ComponentFactory() {
+//     var $Div = $('<div class="component" style="display: none;"><span></span></div>');
+//     $Div.find('span').text('duyi');
+//     $Div.on('cpLeave',function(){
+//         $(this).fadeOut();
+//     })
+//     $Div.on('cpLoad',function(){
+//         $(this).fadeIn();
+//     })
+//     return $Div;
+// }
 
 // 给section添加元素
-$('.section').each(function(index,elem){
-    $(elem).append(ComponentFactory());
-})
+$(".section").each(function (index, elem) {
+  $(elem).append(
+    ComponentFactory({
+      className: "",
+      content: "<span>jQuery-fullPage</span>",
+      css: {
+        // display: "none",
+        opacity: 0,
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        margin: "auto",
+        "text-align": "center",
+        "font-size": "100px",
+        // 'vertical-align':'100%'
+      },
+      event: {
+        click: function () {
+          console.log("click事件生效");
+        },
+      },
+      animation: {
+        animateIn: {
+          opacity: 1,
+          top: 500,
+        },
+        animateOut: {
+          opacity: 0,
+          top: 0,
+        },
+        delay: 100,
+        movement: "swing",
+        inCallback: function () {},
+        outCallback: function () {},
+      },
+    })
+  );
+});
 
 //给section绑定事件
-$('.section').on('_leave',function(){
-    $(this).find('.component').trigger('cpLeave');
-}) 
+$(".section").on("_leave", function () {
+  $(this).find(".component").trigger("cpLeave");
+});
 
-$('.section').on('_load',function(){
-    $(this).find('.component').trigger('cpLoad');
-}) 
-
-
-
-
+$(".section").on("_load", function () {
+  $(this).find(".component").trigger("cpLoad");
+});
